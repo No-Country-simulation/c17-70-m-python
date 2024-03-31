6078926cce6adec7cc9bc92d69aeffd2b6ec0206
Johan <106561192+dantalion-py@users.noreply.github.com>
2024-03-30 22:26:07 -0400

    add configutations doc ⚡️

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -48,11 +48,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'coresapi',
+    'corsheaders',
+    'rest_framework',
+    'login'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -132,3 +138,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORES_ALLOW_ORIGINS = [
+
+]
+REST_FRAMEWORK = {
+    ...:...,
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+}
