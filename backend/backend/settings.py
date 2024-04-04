fb2cb6799004c5b3edad456d56ef99916124a3b0
GustavoNievas <gustavonievasjofre8@gmail.com>
2024-04-04 08:08:49 -0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,8 +76,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 load_dotenv()
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('NAME'),
         'USER': os.getenv('USER'),
