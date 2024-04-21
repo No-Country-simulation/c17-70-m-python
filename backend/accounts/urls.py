89ccac688920ed47c8ce90de63fc683ddb375e43
GustavoNievas <108030873+NievasGustavo@users.noreply.github.com>
2024-04-21 14:23:48 -0300

    Comment Urls :(

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -5,44 +5,55 @@ from rest_framework.documentation import include_docs_urls
 urlpatterns = [
     # URLs para Doctor
     path('doctors/', DoctorListCreate.as_view(), name='doctor-list'),
-    path('doctors/<uuid:pk>/', DoctorRetrieveUpdateDestroy.as_view(), name='doctor-detail'),
+    path('doctors/<uuid:pk>/', DoctorRetrieveUpdateDestroy.as_view(),
+         name='doctor-detail'),
 
     # URLs para Patient
     path('patients/', PatientListCreate.as_view(), name='patient-list'),
-    path('patients/<uuid:pk>/', PatientRetrieveUpdateDestroy.as_view(), name='patient-detail'),
+    path('patients/<uuid:pk>/', PatientRetrieveUpdateDestroy.as_view(),
+         name='patient-detail'),
 
     # URLs para Medicament
     path('medicaments/', MedicamentListCreate.as_view(), name='medicament-list'),
-    path('medicaments/<uuid:pk>/', MedicamentRetrieveUpdateDestroy.as_view(), name='medicament-detail'),
+    path('medicaments/<uuid:pk>/',
+         MedicamentRetrieveUpdateDestroy.as_view(), name='medicament-detail'),
 
     # URLs para Treatment
     path('treatments/', TreatmentListCreate.as_view(), name='treatment-list'),
-    path('treatments/<uuid:pk>/', TreatmentRetrieveUpdateDestroy.as_view(), name='treatment-detail'),
+    path('treatments/<uuid:pk>/',
+         TreatmentRetrieveUpdateDestroy.as_view(), name='treatment-detail'),
 
     # URLs para Recipe
     path('recipes/', RecipeListCreate.as_view(), name='recipe-list'),
-    path('recipes/<uuid:pk>/', RecipeRetrieveUpdateDestroy.as_view(), name='recipe-detail'),
+    path('recipes/<uuid:pk>/', RecipeRetrieveUpdateDestroy.as_view(),
+         name='recipe-detail'),
 
     # URLs para Medical_consultation_history
-    path('medical-consultation-histories/', MedicalConsultationHistoryListCreate.as_view(), name='medical-consultation-history-list'),
-    path('medical-consultation-histories/<uuid:pk>/', MedicalConsultationHistoryRetrieveUpdateDestroy.as_view(), name='medical-consultation-history-detail'),
+    path('medical-consultation-histories/', MedicalConsultationHistoryListCreate.as_view(),
+         name='medical-consultation-history-list'),
+    path('medical-consultation-histories/<uuid:pk>/',
+         MedicalConsultationHistoryRetrieveUpdateDestroy.as_view(), name='medical-consultation-history-detail'),
 
     # URLs para Medical_consultation
-    path('medical-consultations/', MedicalConsultationListCreate.as_view(), name='medical-consultation-list'),
-    path('medical-consultations/<uuid:pk>/', MedicalConsultationRetrieveUpdateDestroy.as_view(), name='medical-consultation-detail'),
+    path('medical-consultations/', MedicalConsultationListCreate.as_view(),
+         name='medical-consultation-list'),
+    path('medical-consultations/<uuid:pk>/',
+         MedicalConsultationRetrieveUpdateDestroy.as_view(), name='medical-consultation-detail'),
 
     # URLs para Administrator
-    path('administrators/', AdministratorListCreate.as_view(), name='administrator-list'),
-    path('administrators/<uuid:pk>/', AdministratorRetrieveUpdateDestroy.as_view(), name='administrator-detail'),
+    path('administrators/', AdministratorListCreate.as_view(),
+         name='administrator-list'),
+    path('administrators/<uuid:pk>/',
+         AdministratorRetrieveUpdateDestroy.as_view(), name='administrator-detail'),
 
     # URLs para Medical_consultation
     path('users/', UserListCreate.as_view(), name='user-list'),
     path('users/<uuid:pk>/', UserRetrieveUpdateDestroy.as_view(), name='user-detail'),
-  
+
     # views
-    #path('api/hello/', views.home, name="hello"),
+    """ #path('api/hello/', views.home, name="hello"),
     path('patient/<uuid:pk>/pdf', GeneratePdf.as_view(), name='patient-pdf'),
     
-    path('documentation/', include_docs_urls(title=f"API doc")),
+    path('documentation/', include_docs_urls(title=f"API doc")),"""
 
 ]
