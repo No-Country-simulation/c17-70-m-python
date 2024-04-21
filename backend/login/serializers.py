95566af19477ddcc11096fd9ba422eebabee0ff1
GustavoNievas <108030873+NievasGustavo@users.noreply.github.com>
2024-04-21 20:43:22 -0300

    Bug fix in CustomUserSerializer

--- backend/login/serializers.py
+++ backend/login/serializers.py
@@ -6,4 +6,4 @@ class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'email', 'user_photo',
-                  'groups', 'birthdate', 'country', 'phone_number', 'gender', 'user_permissions', 'role']
+                  'groups', 'birthdate', 'country', 'phone_number', 'gender', 'user_permissions']
