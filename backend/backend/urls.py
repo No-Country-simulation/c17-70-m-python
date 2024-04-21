e25b32391cbcf792234199170c54d7cd1bc8f2a9
GustavoNievas <108030873+NievasGustavo@users.noreply.github.com>
2024-04-21 18:10:17 -0300

    add urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/auth/', include('login.urls')),
+    path('api/appointments/', include('appointments.urls')),
     # path('lobby/', include('videochat.urls')),
     # path('', home, name='home'),
 
