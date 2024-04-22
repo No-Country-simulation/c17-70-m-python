868580d8022822fc667f67a435d9685029c356d4
GustavoNievas <108030873+NievasGustavo@users.noreply.github.com>
2024-04-22 18:56:13 -0300

    return changes in inital

--- backend/appointments/migrations/0001_initial.py
+++ backend/appointments/migrations/0001_initial.py
@@ -19,27 +19,21 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='WorkShift',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4,
-                 editable=False, primary_key=True, serialize=False)),
+                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
                 ('date', models.DateField(default=django.utils.timezone.now)),
                 ('start_time', models.TimeField()),
                 ('end_time', models.TimeField()),
-                ('doctor', models.ForeignKey(
-                    default=1, on_delete=django.db.models.deletion.CASCADE, to='accounts.doctor')),
+                ('doctor', models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='accounts.doctor')),
             ],
         ),
         migrations.CreateModel(
             name='Appointment',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4,
-                 editable=False, primary_key=True, serialize=False)),
-                ('start_time', models.TimeField(
-                    default=appointments.models.current_time)),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('start_time', models.TimeField(default=appointments.models.current_time)),
                 ('cancelled', models.BooleanField(default=False)),
-                ('patient', models.OneToOneField(blank=True, null=True,
-                 on_delete=django.db.models.deletion.CASCADE, to='accounts.patient')),
-                ('work_shift', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
-                 related_name='appointments', to='appointments.workshift')),
+                ('patient', models.OneToOneField(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='accounts.patient')),
+                ('work_shift', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='appointments', to='appointments.workshift')),
             ],
             options={
                 'verbose_name': 'Appointment',
