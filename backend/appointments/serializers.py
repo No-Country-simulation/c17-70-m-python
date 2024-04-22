846d0bea5f2819ad8170da20ffd24e431d7b55d8
GustavoNievas <108030873+NievasGustavo@users.noreply.github.com>
2024-04-22 18:04:20 -0300

    Add docstring and check date in appoiments

--- backend/appointments/serializers.py
+++ backend/appointments/serializers.py
@@ -27,13 +27,14 @@ class WorkShiftSerializer(serializers.ModelSerializer):
 
 class AppointmentSerializer(serializers.ModelSerializer):
     patient = PatientSerializer(read_only=True)
+    date = serializers.DateField(read_only=True)
     start_time = serializers.TimeField(read_only=True)
     doctor = AppoimentmentDoctorSerializer(
         read_only=True, source='work_shift.doctor')
 
     class Meta:
         model = Appointment
-        fields = ['id', 'patient', 'start_time',
+        fields = ['id', 'patient', 'date', 'start_time',
                   'cancelled', 'doctor']
         extra_kwargs = {
             'patient': {'write_only': False, 'read_only': True},
--- backend/appointments/views.py
+++ backend/appointments/views.py
@@ -9,6 +9,20 @@ from .serializers import AppointmentSerializer, WorkShiftSerializer
 
 
 class WorkShiftViewSet(viewsets.ModelViewSet):
+    """
+    VistaSet para administrar turnos de trabajo.
+
+    Esta clase proporciona operaciones CRUD para administrar turnos de trabajo. Utiliza la clase
+    `WorkShiftSerializer` para la serialización y filtrado.
+
+    Atributos:
+        queryset (QuerySet): El QuerySet de todos los turnos de trabajo.
+        serializer_class (WorkShiftSerializer): La clase serializadora para los turnos de trabajo.
+        permission_classes (list): La lista de clases de permisos necesarias para acceder a la vista.
+
+    Métodos:
+        create(self, request, *args, **kwargs)
+    """
     queryset = WorkShift.objects.all()
     serializer_class = WorkShiftSerializer
     permission_classes = [IsDoctor]
@@ -17,13 +31,28 @@ class WorkShiftViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        workshift = serializer.instance
+        workshift = self.get_serializer().instance
         workshift.create_appointments()
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class AppointmentViewSet(viewsets.ModelViewSet):
+    """
+    VistaSet para administrar citas.
+
+    Esta clase proporciona operaciones CRUD para administrar citas. Utiliza la clase `AppointmentSerializer`
+    para la serialización y filtrado.
+
+    Atributos:
+        queryset (QuerySet): El QuerySet de todas las citas.
+        serializer_class (AppointmentSerializer): La clase serializadora para las citas.
+        filter_backends (list): La lista de backends de filtro que se utilizarán.
+        filter_fields (list): La lista de campos que se pueden filtrar.
+        permission_classes (list): La lista de clases de permisos necesarias para acceder a la vista.
+
+    Métodos:
+        get_queryset(): Devuelve el QuerySet filtrado basado en el paciente y la especialidad.
+    """
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
     filter_backends = [DjangoFilterBackend,
@@ -41,32 +70,81 @@ class AppointmentViewSet(viewsets.ModelViewSet):
 
 
 class BookAppointmentViewSet(viewsets.ModelViewSet):
+    """
+    VistaSet para reservar una cita.
+
+    Esta clase proporciona una operación de creación para reservar una cita
+    utilizando el ID de la cita disponible. El usuario debe estar autenticado
+    como paciente para poder reservar una cita.
+
+    Atributos:
+        queryset (QuerySet): El QuerySet de todas las citas.
+        serializer_class (AppointmentSerializer): La clase serializadora para las citas.
+        permission_classes (list): La lista de clases de permisos necesarias para acceder a la vista.
+
+    Métodos:
+        create(self, request, *args, **kwargs): Reserva una cita utilizando el ID de la cita disponible.
+    """
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
     permission_classes = [IsPatient]
 
     def create(self, request, *args, **kwargs):
+        """
+        Reserva una cita utilizando el ID de la cita disponible. El usuario
+        debe estar autenticado como paciente para poder reservar una cita.
+
+        El request debe incluir el campo 'appointment_id', el cual corresponde
+        al ID de la cita disponible que se desea reservar.
+
+        Devuelve la cita actualizada con el paciente autenticado.
+        """
         appointment_id = request.data.get('appointment_id')
         if not appointment_id:
-            return Response({'error': 'Appointment ID is required.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'appointment_id es requerido.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
         try:
             appointment = Appointment.objects.get(
                 id=appointment_id, patient=None)
-
         except ObjectDoesNotExist:
-            return Response({'error': 'Appointment not found or already booked.'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'error': 'Cita no encontrada o ya reservada.'},
+                            status=status.HTTP_404_NOT_FOUND)
 
         patient = request.user.patient
         if patient:
+            # Verificar que no haya otra cita con el mismo doctor a esa hora
+            if Appointment.objects.filter(patient=patient,
+                                          work_shift=appointment.work_shift).exists():
+                return Response(
+                    {'error': 'El paciente ya tiene una cita con el mismo doctor a esa hora.'},
+                    status=status.HTTP_400_BAD_REQUEST)
+
             appointment.patient = patient
             appointment.save()
+
             serializer = self.get_serializer(appointment)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response({'error': 'Patient not found.'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'error': 'Paciente no encontrado.'}, status=status.HTTP_404_NOT_FOUND)
 
 
 class PatientAppointmentViewSet(viewsets.ModelViewSet):
+    """
+    VistaSet para obtener las citas de un paciente.
+
+    Esta clase proporciona operaciones CRUD para obtener las citas de un paciente
+    autenticado. Utiliza la clase `AppointmentSerializer` para la serialización y
+    filtrado.
+
+    Atributos:
+        queryset (QuerySet): El QuerySet de todas las citas.
+        serializer_class (AppointmentSerializer): La clase serializadora para las citas.
+        permission_classes (list): La lista de clases de permisos necesarias para acceder a la vista.
+
+    Métodos:
+        get_queryset(): Devuelve el QuerySet filtrado por el paciente autenticado.
+    """
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
     permission_classes = [IsPatient]
