01ca7f71f945348ab93a2e49601b364d90d942e6
GustavoNievas <108030873+NievasGustavo@users.noreply.github.com>
2024-04-23 15:10:11 -0300

    CSRF configure

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -49,6 +49,10 @@ CORS_ALLOWED_ORIGINS = [
 
 SESSION_COOKIE_SAMESITE = False
 
+CSRF_COOKIE_SAMESITE = False
+
+CSRF_COOKIE_SECURE = True
+
 SESSION_COOKIE_SECURE = True
 
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'c17-70-m-python-production.up.railway.app']
