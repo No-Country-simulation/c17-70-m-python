2398ddc8599f381da4d949fc4eee957b42f183dc
GustavoNievas <108030873+NievasGustavo@users.noreply.github.com>
2024-04-22 21:37:36 -0300

    Update models.py

--- backend/appointments/models.py
+++ backend/appointments/models.py
@@ -83,4 +83,4 @@ class Appointment(models.Model):
     class Meta:
         verbose_name = 'Appointment'
         verbose_name_plural = 'Appointments'
-        unique_together = ['start_time']
+        unique_together = ([('start_time')])
