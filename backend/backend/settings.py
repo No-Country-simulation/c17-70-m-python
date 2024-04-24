a0246d653e32e04d2391cc2731bb4ac06cd0ad5f
GustavoNievas <108030873+NievasGustavo@users.noreply.github.com>
2024-04-24 08:19:45 -0300

    add

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -90,6 +90,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework.authtoken',
     'dj_rest_auth',
     'django.contrib.sites',
     'allauth',
--- backend/login/urls.py
+++ backend/login/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
