fb865b2475f2bed47c78294701570e900314201f
GustavoNievas <108030873+NievasGustavo@users.noreply.github.com>
2024-04-24 09:23:34 -0300

    Try ajust cors

--- backend/login/views.py
+++ backend/login/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.http import JsonResponse
-from django.contrib.auth import authenticate, login, logout
 from rest_framework import status
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -11,9 +11,11 @@ from dj_rest_auth.registration.views import SocialLoginView
 from .serializers import CustomUserSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from rest_framework_simplejwt.views import TokenObtainPairView
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenError
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
 
 User = get_user_model()
 
@@ -36,32 +38,21 @@ class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
     permission_classes = [AllowAny]
 
+    @csrf_exempt
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise PermissionDenied(str(e)) from e
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
 
 class GoogleLogin(SocialLoginView):
     permission_classes = [AllowAny]
     adapter_class = GoogleOAuth2Adapter
     callback_url = settings.SOCIAL_AUTH_GOOGLE_OAUTH2_REDIRECT_URI
     client_class = OAuth2Client
-
-
-class LogoutView(APIView):
-    permission_classes = [AllowAny]
-    """Maneja la funcionalidad de cierre de sesión del usuario.
-    Atributos:
-        - Permiso_clases (lista): lista de clases de permisos requeridas para esta vista..
-    """
-
-    def post(self, request):
-        """
-        Cerrar la sesión del usuario del sistema
-        Parámetros:
-            Solicitud: el objeto de la solicitud
-        Devoluciones:
-            JsonResponse: un mensaje de éxito
-        """
-        logout(request)
-        return JsonResponse({'message': 'Logout exitoso'})
-
 
 class HomeView(APIView):
     authentication_classes = [JWTAuthentication]
