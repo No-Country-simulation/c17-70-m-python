df41d153861046fb7364e3c471ea60025aec8b59
GustavoNievas <108030873+NievasGustavo@users.noreply.github.com>
2024-04-24 11:55:44 -0300

    Token life adjustment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -203,7 +203,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 SOCIALACCOUNT_ADAPTER = 'login.adapters.CustomSocialAccountAdapter'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=180),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days= 1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=50),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
