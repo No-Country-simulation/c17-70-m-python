9749fcfc32b890b877147dcf44f13dfb95a3adb9
GustavoNievas <108030873+NievasGustavo@users.noreply.github.com>
2024-04-24 09:32:42 -0300

    error fix

--- backend/login/urls.py
+++ backend/login/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('login/', views.CustomTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('dj-rest-auth/google/', views.GoogleLogin.as_view(), name='google_login'),
