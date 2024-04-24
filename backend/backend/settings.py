9d9ad7c7827e84c796897301f801f8b3e2c10330
GustavoNievas <108030873+NievasGustavo@users.noreply.github.com>
2024-04-23 21:37:52 -0300

    add allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,7 +57,8 @@ SESSION_COOKIE_SECURE = True
 
 SESSION_COOKIE_SAMESITE = 'None'
 
-# ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'c17-70-m-python-production.up.railway.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'c17-70-m-python-production.up.railway.app',
+                 'c17-70-m-python-git-71-f013-ap-251096-francoespinozavs-projects.vercel.app']
 
 ALLOWED_HOSTS = ["*"]
 
