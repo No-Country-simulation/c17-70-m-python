4ed27721b4ec02ef4253617782660819529130e8
GustavoNievas <108030873+NievasGustavo@users.noreply.github.com>
2024-04-25 14:06:41 -0300

    update patch method x2

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -93,14 +93,14 @@ class PatientListCreate(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         return serializer.save()
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(
-            instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
+    def update(self, instance, validated_data):
+        email = validated_data.get('email', instance.email)
+        password = validated_data.get('password', instance.password)
+        instance.email = email
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
     def perform_update(self, serializer):
         serializer.save()
